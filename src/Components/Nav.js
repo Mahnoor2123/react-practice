a4b46e93a60843530ceed7e785930e29caa52c2d
Mahnoor2123 <mahikhan2999@gmail.com>
2023-10-18 09:08:51 +0500

    working on react basic

---
+++ src/Components/Nav.js
@@ -0,0 +1,19 @@
+import React, { useLayoutEffect } from 'react'
+import "./Nav.css";
+function Nav() {
+  return (
+    <div className='Navbar'>
+        <ul>
+        <li>Home</li>
+        <li>About Me</li>
+        <li>Hobbies</li>
+        <li>Wishes</li>
+         <li>History</li>
+</ul>
+<button>Contact Us</button>
+    </div>
+    
+  )
+}
+
+export default Nav
---
+++ src/Components/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Sidebar(){
+  return (
+    <h1>This is the Sidebar</h1>
+  )
+}
+
+export default Sidebar
